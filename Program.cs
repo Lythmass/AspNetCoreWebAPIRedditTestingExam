5739753de70b25d693b51b71ad1bc07222092ea5
Anri Kezeroti <AKezeroti@abacusnext.com>
2024-02-25 20:56:30 +0400

    Add Frontend to read Posts

--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplcationDBContext>();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policyBuilder => policyBuilder.AllowAnyOrigin()));
+/*that allows or restricts web applications running in a browser from making requests to a server hosted on a different origin.
+ * An "origin" is defined by the scheme (protocol), host (domain), and port of a URL, meaning that requests across different domains, schemes, or ports are considered cross-origin.
+ https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-8.0
+https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+*/
+
+/*code configures the CORS policy to allow requests from any origin,
+which is useful for development environments or specific applications that need to be accessible from multiple or unknown origins.
+However, allowing any origin in a production environment can expose your application to certain security risks, such as cross-site request forgery (CSRF) or data leakage.*/
 
 var app = builder.Build();
 
@@ -20,6 +30,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+// add the Middleware to app pipeline
 
 app.UseAuthorization();
 
