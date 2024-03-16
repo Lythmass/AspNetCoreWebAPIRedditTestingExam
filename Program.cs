ed9ecaf19db2f7f08e19291b54556c56338887d2
Anri Kezeroti <AKezeroti@abacusnext.com>
2024-03-16 21:52:38 +0400

    Solve Infinite Cycles problem By specifing to Serilizer to Ignore Cycles

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplcationDBContext>(options => {
